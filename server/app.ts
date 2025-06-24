625b03e8796969916cba2813545d1e77e8d83b5a
HARSH SINGH <harshsingh.bar@gmail.com>
2025-06-24 15:38:46 +0530

    CORS changes

--- server/app.ts
+++ server/app.ts
@@ -24,7 +24,7 @@ app.use(express.json({ limit: "50mb" }));
 // cors => cross origin resource sharing
 app.use(
   cors({
-    origin: process.env.ORIGIN,
+    origin: '*',
     credentials: true,
   })
 );
