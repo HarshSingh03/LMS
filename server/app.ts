f3696bfd0f59b41d54f6a6cd98ea6201725c55ee
HARSH SINGH <harshsingh.bar@gmail.com>
2025-06-24 16:13:14 +0530

    CORS origin from env

--- server/app.ts
+++ server/app.ts
@@ -24,7 +24,7 @@ app.use(express.json({ limit: "50mb" }));
 // cors => cross origin resource sharing
 app.use(
   cors({
-    origin: '*',
+    origin: process.env.ORIGIN,
     credentials: true,
   })
 );
