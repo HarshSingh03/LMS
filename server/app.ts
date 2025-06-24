4b7d11ecf4b9f172a5f1bb99a4d2826010c99e37
HARSH SINGH <harshsingh.bar@gmail.com>
2025-06-24 14:48:14 +0530

    Chatbot integrated

--- server/app.ts
+++ server/app.ts
@@ -24,7 +24,7 @@ app.use(express.json({ limit: "50mb" }));
 // cors => cross origin resource sharing
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: process.env.ORIGIN,
     credentials: true,
   })
 );
